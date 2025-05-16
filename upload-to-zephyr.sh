e4968a7c7c287355ce8b15ec360443bb17127358
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2025-05-16 12:31:16 +0200

    Create upload-to-zephyr.sh

---
+++ upload-to-zephyr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API_TOKEN="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL3Rlc3RtYW1hcHAuYXRsYXNzaWFuLm5ldCIsInVzZXIiOnsiYWNjb3VudElkIjoiNzEyMDIwOmE4MDJjZmJiLWJhN2YtNDNhNi1hMWUwLTA3ZWE0ZjA5MTdlYiIsInRva2VuSWQiOiI4ZGIzNjY5YS02ZGI2LTQwOTktYWU1OS00OWJhZTIwNGIyMGYifX0sImlzcyI6ImNvbS5rYW5vYWgudGVzdC1tYW5hZ2VyIiwic3ViIjoiMjIxZjQyMGMtZjJkNC0zYzQwLTg1OTQtMjU3MzFmZWQyZjY2IiwiZXhwIjoxNzc4NzYzNzUwLCJpYXQiOjE3NDcyMjc3NTB9.oPFZvlpsTU3XTra9I_3TC3wJuezd_xCTI48t3wlkhLw"
+PROJECT_KEY="My Scrum Project"
+FILE_PATH="target/cucumber-report.json"
+TEST_CYCLE_NAME="Jenkins Test Cycle"
+ZEPHYR_URL="https://api.zephyrscale.smartbear.com"
+
+echo "Uploading report to Zephyr..."
+
+curl -X POST "$ZEPHYR_URL/v2/automations/executions/cucumber" \
+  -H "Authorization: Bearer $API_TOKEN" \
+  -H "Content-Type: multipart/form-data" \
+  -F "file=@$FILE_PATH" \
+  -F "projectKey=$PROJECT_KEY" \
+  -F "testCycle=$TEST_CYCLE_NAME" \
+  -F "autoCreateTestCases=true"

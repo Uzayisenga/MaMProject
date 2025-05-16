5341bc1aa820a698b8df1c58c93639ff4a8e9f88
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2025-05-16 13:12:11 +0200

    Update upload-to-zephyr.bat

--- upload-to-zephyr.bat
+++ upload-to-zephyr.bat
@@ -1,17 +1,20 @@
-#!/bin/bash
+@echo off
+setlocal
 
-API_TOKEN="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL3Rlc3RtYW1hcHAuYXRsYXNzaWFuLm5ldCIsInVzZXIiOnsiYWNjb3VudElkIjoiNzEyMDIwOmE4MDJjZmJiLWJhN2YtNDNhNi1hMWUwLTA3ZWE0ZjA5MTdlYiIsInRva2VuSWQiOiI4ZGIzNjY5YS02ZGI2LTQwOTktYWU1OS00OWJhZTIwNGIyMGYifX0sImlzcyI6ImNvbS5rYW5vYWgudGVzdC1tYW5hZ2VyIiwic3ViIjoiMjIxZjQyMGMtZjJkNC0zYzQwLTg1OTQtMjU3MzFmZWQyZjY2IiwiZXhwIjoxNzc4NzYzNzUwLCJpYXQiOjE3NDcyMjc3NTB9.oPFZvlpsTU3XTra9I_3TC3wJuezd_xCTI48t3wlkhLw"
-PROJECT_KEY="My Scrum Project"
-FILE_PATH="target/cucumber-report.json"
-TEST_CYCLE_NAME="Jenkins Test Cycle"
-ZEPHYR_URL="https://api.zephyrscale.smartbear.com"
+set API_TOKEN=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL3Rlc3RtYW1hcHAuYXRsYXNzaWFuLm5ldCIsInVzZXIiOnsiYWNjb3VudElkIjoiNzEyMDIwOmE4MDJjZmJiLWJhN2YtNDNhNi1hMWUwLTA3ZWE0ZjA5MTdlYiIsInRva2VuSWQiOiI4ZGIzNjY5YS02ZGI2LTQwOTktYWU1OS00OWJhZTIwNGIyMGYifX0sImlzcyI6ImNvbS5rYW5vYWgudGVzdC1tYW5hZ2VyIiwic3ViIjoiMjIxZjQyMGMtZjJkNC0zYzQwLTg1OTQtMjU3MzFmZWQyZjY2IiwiZXhwIjoxNzc4NzYzNzUwLCJpYXQiOjE3NDcyMjc3NTB9.oPFZvlpsTU3XTra9I_3TC3wJuezd_xCTI48t3wlkhLw
+set PROJECT_KEY=My Scrum Project
+set FILE_PATH=target/cucumber-report.json
+set TEST_CYCLE_NAME=Jenkins Test Cycle
+set ZEPHYR_URL=https://api.zephyrscale.smartbear.com
 
-echo "Uploading report to Zephyr..."
+echo Uploading report to Zephyr...
 
-curl -X POST "$ZEPHYR_URL/v2/automations/executions/cucumber" \
-  -H "Authorization: Bearer $API_TOKEN" \
-  -H "Content-Type: multipart/form-data" \
-  -F "file=@$FILE_PATH" \
-  -F "projectKey=$PROJECT_KEY" \
-  -F "testCycle=$TEST_CYCLE_NAME" \
+curl -X POST "%ZEPHYR_URL%/v2/automations/executions/cucumber" ^
+  -H "Authorization: Bearer %API_TOKEN%" ^
+  -H "Content-Type: multipart/form-data" ^
+  -F "file=@%FILE_PATH%" ^
+  -F "projectKey=%PROJECT_KEY%" ^
+  -F "testCycle=%TEST_CYCLE_NAME%" ^
   -F "autoCreateTestCases=true"
+
+endlocal

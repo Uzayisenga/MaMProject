c40809db07e84e8204916280c5e867da5dfbc2aa
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2025-05-22 10:08:59 +0200

    Update upload-to-zephyr.bat

--- upload-to-zephyr.bat
+++ upload-to-zephyr.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Uploading report to Zephyr...
 
-REM Set your Zephyr token
-set ZEPHYR_TOKEN=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL3Rlc3RtYW1hcHAuYXRsYXNzaWFuLm5ldCIsInVzZXIiOnsiYWNjb3VudElkIjoiNzEyMDIwOmE4MDJjZmJiLWJhN2YtNDNhNi1hMWUwLTA3ZWE0ZjA5MTdlYiIsInRva2VuSWQiOiJjOTQxZWM2NS03MGJkLTRhNDctODRlNy1iOGZiNzE3ZDJmNTIifX0sImlzcyI6ImNvbS5rYW5vYWgudGVzdC1tYW5hZ2VyIiwic3ViIjoiMjIxZjQyMGMtZjJkNC0zYzQwLTg1OTQtMjU3MzFmZWQyZjY2IiwiZXhwIjoxNzc5Mjg0MDQ1LCJpYXQiOjE3NDc3NDgwNDV9.UGQndUJQWXYwLRtFv9h22Q3e5fL8LdIP3i0JDInP58Q
+REM The ZEPHYR_TOKEN environment variable is provided by Jenkins' withCredentials block.
+REM DO NOT hardcode the token here, as it overwrites the secure credential.
 
 REM Look for any Surefire report in target/surefire-reports
 for %%f in (target\surefire-reports\TEST-*.xml) do (
